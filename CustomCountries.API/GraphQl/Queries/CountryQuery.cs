4840293d0ca47bcf24ad172988af0558f6bfdb70
rafaelbitencourt <rafael_silbit@hotmail.com>
2021-03-06 17:38:01 -0300

    Filtro

--- CustomCountries.API/GraphQl/Queries/CountryQuery.cs
+++ CustomCountries.API/GraphQl/Queries/CountryQuery.cs
@@ -1,11 +1,13 @@
 ﻿using CustomCountries.API.Models;
 using HotChocolate;
+using HotChocolate.Types;
 using System.Linq;
 
 namespace CustomCountries.API.Queries
 {
     public class CountryQuery
     {
+        [UseFiltering]
         public IQueryable<Country> GetCountries([Service]DataBaseContext _dbContext) =>
             _dbContext.Countries;
     }
