bc284805fafb57a3b94b8f46c3666e57948b8523
rafaelbitencourt <rafael_silbit@hotmail.com>
2021-03-07 17:38:12 -0300

    Connection String no AppSettings

--- CustomCountries.API/Models/DataBaseContext.cs
+++ CustomCountries.API/Models/DataBaseContext.cs
@@ -1,16 +1,22 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Npgsql;
 
 namespace CustomCountries.API.Models
 {
     public class DataBaseContext : DbContext
     {
+        IConfiguration _configuration;
         public DbSet<Country> Countries { get; set; }
 
-        private const string StringConnection = "Host=ec2-54-90-13-87.compute-1.amazonaws.com;Port=5432;Database=dcjrgssurdame0;User Id=rjbvdhzkqpeqci;Password=b5255cfce087f217907f291058cca98c4770e877ecb7032a76b2f6c8838e7a3b;Pooling=true;SSL Mode=Require;TrustServerCertificate=True;";
+        public DataBaseContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
-            options.UseNpgsql(new NpgsqlConnection(StringConnection));
+            options.UseNpgsql(new NpgsqlConnection(_configuration.GetConnectionString("CustomCountriesDB")));
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
